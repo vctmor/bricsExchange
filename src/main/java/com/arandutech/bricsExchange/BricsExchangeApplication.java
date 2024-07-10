15b5030bb4644193a3b406941022451372f5f31e
victor_moreira <victormoreira@vivaldi.net>
2024-07-10 10:04:09 -0300

    first commit

---
+++ src/main/java/com/arandutech/bricsExchange/BricsExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.arandutech.bricsExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BricsExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BricsExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arandutech/bricsExchange/BricsExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arandutech.bricsExchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BricsExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
